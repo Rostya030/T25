a49d2b709c3a9c5a978c24bf5188d282f27741bd
Rostyaslaww) <rostislaww1@gmail.com>
2025-08-30 18:29:39 +0300

    дз

--- js/lib.js
+++ js/lib.js
@@ -1,10 +1,20 @@
-const firstName = "megapro";
-console.log(firstName);
-let lastName = "coderultra";
-console.log(lastName);
-console.log(typeof lastName);
-const number = 993;
-console.log(number);
-console.log(number);
-const number2 = "true";
-console.log(typeof number2);
+const age = 14.5;
+console.log(age);
+let name = "Rostya2012";
+console.log(name);
+const isStudent = 5 > 2;
+console.log(isStudent);
+const myString = "'Україна — це доля, це земля, це небо, це народ, це ти, це я, це ми!— Василь Симоненко'";
+console.log(myString);
+const myNumber = "";
+console.log(myNumber);
+const myNull = null;
+console.log(myNull);
+console.log(typeof myNull);
+
+const promtOne = prompt("Введите свое место проживания");
+console.log(promtOne);
+
+const isComing = confirm("Цей сайт може бути не безпечним!! (ваш антивирус ОМегаПроВирусъ)");
+
+console.log(isComing);
