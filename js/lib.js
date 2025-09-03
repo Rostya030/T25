befa1f40dbc2ea21e6293165a8d58485ad671caf
Rostyaslaww) <rostislaww1@gmail.com>
2025-09-03 22:14:47 +0300

    final D\Z

--- js/lib.js
+++ js/lib.js
@@ -1,13 +1,20 @@
 const age = 14.5;
 console.log(age);
+
 let name = "Rostya2012";
 console.log(name);
+
 const isStudent = 5 > 2;
 console.log(isStudent);
+
 const myString = "'Україна — це доля, це земля, це небо, це народ, це ти, це я, це ми!— Василь Симоненко'";
 console.log(myString);
-const myNumber = "";
-console.log(myNumber);
+
+const myNumber = prompt ("введiть перше число",);
+const youNumber = prompt ("введiь друге число",)
+const allNumber = myNumber + youNumber
+console.log(allNumber);
+
 const myNull = null;
 console.log(myNull);
 console.log(typeof myNull);
@@ -16,5 +23,21 @@ const promtOne = prompt("Введiть свое мiсто");
 console.log(promtOne);
 
 const isComing = confirm("Цей сайт може бути не безпечним!! (ваш антивирус ОМегаПроВирусъ)");
-
 console.log(isComing);
+
+const message = "Hi, helo-o-o...."
+alert(message)
+
+const myAge = 13 
+console.log(myAge);
+
+const friendAge = 14
+console.log(friendAge);
+
+const allAge = myAge + friendAge
+console.log(allAge);
+
+const numberTumber = 990
+const plusNumberTumber = 10
+const allTumber = numberTumber + plusNumberTumber
+console.log(allTumber);
