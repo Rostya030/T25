9d1e91080bce7b3659ffd7254b8a683d56a8924f
Rostyaslaww) <rostislaww1@gmail.com>
2025-09-04 15:48:32 +0300

    Update lib.js

--- js/lib.js
+++ js/lib.js
@@ -4,29 +4,26 @@ console.log(age);
 let name = "Rostya2012";
 console.log(name);
 
-const isStudent = 5 > 2;
+const isStudent = true;
 console.log(isStudent);
 
 const myString = "'Україна — це доля, це земля, це небо, це народ, це ти, це я, це ми!— Василь Симоненко'";
 console.log(myString);
 
-const myNumber = prompt ("введiть перше число",);
-const youNumber = prompt ("введiь друге число",);
-const allNumber = myNumber + youNumber;
+const myNumber = 1101;
+const allNumber = myNumber + 10;
 console.log(allNumber);
 
 const myNull = null;
 console.log(myNull);
 console.log(typeof myNull);
 
-const promtOne = prompt("Введiть свое мiсто");
+const userName = prompt("Введiть свое мiсто");
+const alertOne = alert (userName)
 console.log(promtOne);
 
-const isComing = confirm("Цей сайт може бути не безпечним!! (ваш антивирус ОМегаПроВирусъ)");
-console.log(isComing);
-
-const message = "Hi, helo-o-o....";
-alert(message);
+const userAnswer = confirm("Цей сайт може бути не безпечним!! (ваш антивирус ОМегаПроВирусъ)");
+console.log(userAnswer);
 
 const myAge = 13;
 console.log(myAge);
@@ -36,8 +33,3 @@ console.log(friendAge);
 
 const allAge = myAge + friendAge;
 console.log(allAge);
-
-const numberTumber = 990;
-const plusNumberTumber = 10;
-const allTumber = numberTumber + plusNumberTumber;
-console.log(allTumber);
