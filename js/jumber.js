8048f919c6ec16abc437e960af4380e89c813d5f
Rostyaslaww) <rostislaww1@gmail.com>
2025-09-05 19:32:44 +0300

    Update jumber.js

--- js/jumber.js
+++ js/jumber.js
@@ -1,14 +1,43 @@
-// const a = 15;
-// const b = 5;
-// const c = a % b;
-// console.log(c);
-
-// const a = 14;
-// const b = 14;
-// const c = a <= b;
-// console.log(c);
-
-const a = 5;
-const b = "5";
-const c = a == b;
+const a = 15;
+const b = 5;
+const c = a > b;
 console.log(c);
+
+const d = 14;
+const e = 14;
+const f = a > b;
+console.log(c);
+
+const g = 5;
+const m = "5";
+const n = a !== b;
+console.log(c);
+
+const userAge = Number(prompt("Скільки вам років"));
+console.log(Number.isNaN(userAge));
+console.log(typeof userAge);
+
+const number = Number(prompt("Введіть якесь число"));
+console.log(number);
+console.log(typeof number);
+
+console.dir(Number);
+
+console.log(0.1 + 0.2);
+console.log((0.1 * 10 + 0.2 * 10) / 10);
+console.log(Number((0.1 + 0.2).toFixed(3)));
+
+
+const l = 5;
+const o = "5";
+const p = a + b;
+console.log(c);
+console.log(typeof c);
+const h = "Rostislaww1";
+const j = "993+13+67+9.5";
+console.log(d + e);
+
+const junper = 456
+const convertedNumber = junper.toString();
+console.log(convertedNumber);
+console.log(typeof convertedNumber);
